eb8fdb974e26abf85e1edbe89e6b094b8820c31f
Graham Ollis <perl@wdlabs.com>
2014-12-10 07:52:09 -0500

    more reliable way of loading File::HomeDir::Test

--- t/etc/setup.pl
+++ t/etc/setup.pl
@@ -1,10 +1,14 @@
 use strict;
 use warnings;
 use v5.10;
-eval q{
-  require File::HomeDir::Test;
-  File::HomeDir::Test->import;
-};
+
+BEGIN {
+  unless($INC{'File/HomeDir/Test.pm'})
+  {
+    eval q{ use File::HomeDir::Test };
+    die $@ if $@;
+  }
+}
 
 $ENV{MOJO_HOME} = "$FindBin::Bin";
 $ENV{CLUSTERICIOUS_CONF_DIR} = $ENV{PLUGAUTH_CONF_DIR} // "$FindBin::Bin/etc";
